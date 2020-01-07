73b122ed32ba6fbe122b3feafc985d1e73322168
AidanHsiao <49498006+AidanHsiao@users.noreply.github.com>
2020-01-07 05:11:03 -0800

    Website Rework
    New filling system, js introduced for index page, new font, reworked titles

---
+++ script.js
@@ -0,0 +1,5 @@
+function notReady() {
+	document.getElementById("bannertext").style.opacity = "0";
+	document.getElementById("bannertext").innerHTML = "Unfortunately, this article is not ready yet. This article will be ready when the title isn't red. Come back soon!";
+	document.getElementById("bannertext").style.opacity = "1";
+}
